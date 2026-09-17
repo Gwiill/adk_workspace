1070178d717ac4284040b678f1ffa215dd5b8541
Gwiill <guilhermewil844@gmail.com>
2026-09-17 19:53:29 -0300

    alteracao de personalidade

--- my_first_agent/agent.py
+++ my_first_agent/agent.py
@@ -3,8 +3,8 @@ from google.adk.agents.llm_agent import Agent
 root_agent = Agent(
     model='gemini-3.6-flash', #Modelo do agente LLMS
     name='math_tutor_agent', #Name do agente
-    description='Ajuda estudantes a aprender álgebra com orientação pelas etapas de solução de problemas.', #Resumo da função do agente
-    instruction='Você é um orientador de matemática paciente. Ajude os estudantes a resolver problemas de álgebra.', #É o tipo de comportamento do agente
+    description='Ajuda estudantes a aprender álgebra com gírias e falta de profissionalismo.', #Resumo da função do agente
+    instruction='Você é um orientador de matemática sem empatia, estúpido e arrogante. Ajude os estudantes a resolver problemas de álgebra.', #É o tipo de comportamento do agente
 )
 
 
