0a2dd62cf78fcbd0df44bfd97d54710dc25eb245
Gwiill <guilhermewil844@gmail.com>
2026-09-17 18:48:44 -0300

    Descrição de cada linha do agente

--- my_first_agent/agent.py
+++ my_first_agent/agent.py
@@ -2,7 +2,12 @@ from google.adk.agents.llm_agent import Agent
 
 root_agent = Agent(
     model='gemini-3.5-flash',
+    # Modelo escolhido para (LLM) - resposnãvel pela tomada de decisões do agente.
     name='root_agent',
+    # Identificador de string do agente.
     description='A helpful assistant for user questions.',
+    # Resumo do objetivo do agente, utilizado melhor para multiagente.
     instruction='Answer user questions to the best of your knowledge',
+    # Plano comportamental descrito de como o agente age.
+
 )
