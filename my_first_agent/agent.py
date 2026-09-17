459f4567f46f4226b94301334bc9d757f413f68c
Gwiill <guilhermewil844@gmail.com>
2026-09-17 19:22:32 -0300

    identação

--- my_first_agent/agent.py
+++ my_first_agent/agent.py
@@ -1,20 +1,12 @@
 from google.adk.agents.llm_agent import Agent
 
 root_agent = Agent(
-    model='gemini-3.6-flash',
-
-    # Modelo escolhido para (LLM) - resposnãvel pela tomada de decisões do agente.
-    name='math_tutor_agent',
-
-    # Identificador de string do agente.
-    description='Ajuda estudantes a aprender álgebra com orientação pelas etapas de solução de problemas.',
-
-    # Resumo do objetivo do agente, utilizado melhor para multiagente.
-    instruction='Você é um orientador de matemática paciente. Ajude os estudantes a resolver problemas de álgebra.',
-
-    # Plano comportamental descrito de como o agente age.
-
+    model='gemini-3.6-flash', #Modelo do agente LLMS
+    name='math_tutor_agent', #Name do agente
+    description='Ajuda estudantes a aprender álgebra com orientação pelas etapas de solução de problemas.', #Resumo da função do agente
+    instruction='Você é um orientador de matemática paciente. Ajude os estudantes a resolver problemas de álgebra.', #É o tipo de comportamento do agente
 )
+
 
 #- ✅ Definição específica do papel (orientador de matemática)
 #- ✅ Personalidade clara (paciente)
