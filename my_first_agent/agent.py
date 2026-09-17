a1f057d538253a5d26736a4f901351519e6bb85f
Gwiill <guilhermewil844@gmail.com>
2026-09-17 19:10:27 -0300

    Correção de sintaxe

--- my_first_agent/agent.py
+++ my_first_agent/agent.py
@@ -1,23 +1,21 @@
 from google.adk.agents.llm_agent import Agent
 
 root_agent = Agent(
-    model='gemini-3.5-flash',
+    model='gemini-3.6-flash',
 
     # Modelo escolhido para (LLM) - resposnãvel pela tomada de decisões do agente.
     name='math_tutor_agent',
 
     # Identificador de string do agente.
-    description='Ajuda estudantes a aprender álgebra com orientação pelas etapas de solução de problemas.'
+    description='Ajuda estudantes a aprender álgebra com orientação pelas etapas de solução de problemas.',
 
     # Resumo do objetivo do agente, utilizado melhor para multiagente.
-    instruction='Você é um orientador de matemática paciente. Ajude os estudantes a resolver problemas de álgebra.'
+    instruction='Você é um orientador de matemática paciente. Ajude os estudantes a resolver problemas de álgebra.',
 
     # Plano comportamental descrito de como o agente age.
 
-"""
-- ✅ Definição específica do papel (orientador de matemática)
-- ✅ Personalidade clara (paciente)
-- ✅ Definição do escopo da tarefa (problemas de álgebra)
-"""
-
 )
+
+#- ✅ Definição específica do papel (orientador de matemática)
+#- ✅ Personalidade clara (paciente)
+#- ✅ Definição do escopo da tarefa (problemas de álgebra)
